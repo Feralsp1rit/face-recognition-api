21d26634a40b2fe8df63467e28613f2d0ba51973
Rage <umicevic.zoran1@gmail.com>
2018-10-17 00:45:02 +0200

    updating host

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-encircled-31105',
     user : 'rage',
     password : 'rage',
     database : 'smartbrain'
